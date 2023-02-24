36f1faf5e906ea6317f8efe550df8a48e6d41d4d
Lagostacio <inaciobortolaslago@gmail.com>
2023-02-24 11:07:16 -0300

    teste com twig

--- produtos.php
+++ produtos.php
@@ -6,6 +6,27 @@
 
     $template = $twig->load("produtos.html");
 
+    $produtos = [
+        [
+            'nome' => 'Chinelo',
+            'preco' => 30,
+        ],
+        [
+            'nome' => 'Camiseta',
+            'preco' => 50,
+        ],
+        [
+            'nome' => 'Boné',
+            'preco' => 39.9,
+        ],
+        [
+            'nome' => 'Batmóvel',
+            'preco' => 3696969,
+        ],
+    ];
+
     echo $template->render([
         'title' => 'Produtos',
+        //'produtos' => $produtos,
+        'produtos' => null,
     ]);
